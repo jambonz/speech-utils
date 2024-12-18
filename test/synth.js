7327190471df60ee1043f1e3a90d82050e2d62ef
Quan HL <quan.luuhoang8@gmail.com>
2024-12-18 16:46:24 +0700

    remove audio extension from audio key

--- test/synth.js
+++ test/synth.js
@@ -888,24 +888,15 @@ test('TTS Cache tests', async(t) => {
       language: 'non-existing',
       voice: 'non-existing',
     });
-    t.ok(purgedCountWhenErrored === 0, `purged no records when specified key was not found`);
-    t.ok(error, `error returned when specified key was not found`);
+    t.ok(purgedCountWhenErrored === 0, 'purged no records when specified key was not found');
+    t.ok(error, 'error returned when specified key was not found');
 
     // make sure other tts keys are still there
-    const cached = await client.keys('tts:*')
-    t.ok(cached.length >= 1, `successfully kept all non-specified tts records in cache`);
+    const cached = await client.keys('tts:*');
+    t.ok(cached.length >= 1, 'successfully kept all non-specified tts records in cache');
 
-    // retrieve keys from cache and check the key contains the file extension    
-    let key = cached[0];
-    t.ok(key.includes('mp3'), `tts cache extension shoult be part of the key and equal mp3`);
-
-    process.env.VG_TRIM_TTS_SILENCE = 'true';    
+    process.env.VG_TRIM_TTS_SILENCE = 'true';
     await client.set(makeSynthKey({ vendor: 'azure' }), 'value');
-
-    const r8Keys = await client.keys('tts:r8*');
-    key = r8Keys[0];
-    t.ok(key.includes('r8'), `tts cache extension shoult be part of the key and equal r8`);
-
   } catch (err) {
     console.error(JSON.stringify(err));
     t.end(err);
