92734aaedb3fd6274b96c259c588c7e689905cda
Quan HL <quan.luuhoang8@gmail.com>
2025-08-14 16:38:36 +0700

    wip

--- test/synth.js
+++ test/synth.js
@@ -185,6 +185,7 @@ test('AWS speech synth tests', async(t) => {
       language: 'en-US',
       voice: 'Joey',
       text: 'This is a test.  This is only a test',
+      disableTtsCache: true,
     });
     t.ok(!opts.servedFromCache, `successfully synthesized aws audio to ${opts.filePath}`);
 
